262078b36e997cdc0808473333497a9e2a0ae9a7
rmberrospig <rmberrospig@outlook.com>
2023-01-28 13:06:35 -0500

    initial commit

---
+++ src/main/java/upc/edu/pe/tutorconnect/TutorConnectApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.tutorconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upc/edu/pe/tutorconnect/TutorConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.tutorconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
